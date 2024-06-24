87f9a696b3e3acde13f339f079b9839d63a12eb8
RadityaSanjaya59 <maszzeh59@gmail.com>
2024-06-24 19:41:34 +0700

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,31 @@
-import { createPinia } from 'pinia';
+import { defineStore } from 'pinia';
 
-const pinia = createPinia();
-
-export default pinia;
+export const useActivitiesStore = defineStore('activities', {
+  state: () => ({
+    activities: [],
+    showOnlyIncomplete: false,
+  }),
+  getters: {
+    filteredActivities: (state) => {
+      return state.showOnlyIncomplete
+        ? state.activities.filter(activity => !activity.completed)
+        : state.activities;
+    },
+  },
+  actions: {
+    addActivity(activityName) {
+      if (activityName.trim()) {
+        this.activities.push({ name: activityName, completed: false });
+      }
+    },
+    toggleActivity(index) {
+      this.activities[index].completed = !this.activities[index].completed;
+    },
+    removeActivity(index) {
+      this.activities.splice(index, 1);
+    },
+    filterCompleted() {
+      this.showOnlyIncomplete = !this.showOnlyIncomplete;
+    },
+  },
+});
