00cc8b3b5155de9a2f45b52ec9c6b6ecc8279890
RadityaSanjaya59 <maszzeh59@gmail.com>
2024-06-24 19:41:05 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import pinia from './store';
+import { createPinia } from 'pinia';
 import { Quasar } from 'quasar';
 import 'quasar/src/css/index.sass';
 import 'quasar/src/css/index.sass';
@@ -10,7 +10,7 @@ import 'quasar/src/css/index.sass';
 const app = createApp(App);
 
 app.use(router);
-app.use(pinia);
+app.use(createPinia());
 app.use(Quasar, { plugins: {} });
 
 app.mount('#app');
